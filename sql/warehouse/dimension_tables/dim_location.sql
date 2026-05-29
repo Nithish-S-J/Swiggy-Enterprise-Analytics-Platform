29e3f61c001496e772a9a5e0f70f5faeae979465
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-29 13:22:09 +0530

    Update dim_location.sql

--- sql/warehouse/dimension_tables/dim_location.sql
+++ sql/warehouse/dimension_tables/dim_location.sql
@@ -1,21 +1,31 @@
 DROP TABLE IF EXISTS dbo.dim_location;
 GO
 
-CREATE TABLE dbo.dim_location (
-    location_id INT NOT NULL,
+CREATE TABLE dbo.dim_location
+(
+    location_key INT,
     city VARCHAR(100),
     area VARCHAR(100)
 );
 GO
 
 INSERT INTO dbo.dim_location
-SELECT 
-    ROW_NUMBER() OVER (ORDER BY city, area) AS location_id,
+
+SELECT
+
+    ROW_NUMBER() OVER
+    (
+        ORDER BY city, area
+    ) AS location_key,
+
     city,
     area
-FROM (
-    SELECT DISTINCT city, area 
-    FROM dbo.silver_restaurants 
-    WHERE city IS NOT NULL
-) src;
+
+FROM
+(
+    SELECT DISTINCT
+        city,
+        area
+    FROM dbo.silver_restaurants
+) x;
 GO

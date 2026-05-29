45d8cc4f9285e120bb13267a3595fd9b1f18db10
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-29 13:22:52 +0530

    Update fact_orders.sql

--- sql/warehouse/fact_tables/fact_orders.sql
+++ sql/warehouse/fact_tables/fact_orders.sql
@@ -1,16 +1,84 @@
-CREATE TABLE fact_orders AS
+DROP TABLE IF EXISTS dbo.fact_orders;
+GO
+
+CREATE TABLE dbo.fact_orders
+(
+    order_id VARCHAR(100),
+
+    customer_id VARCHAR(100),
+
+    restaurant_key INT,
+
+    platform VARCHAR(50),
+
+    order_timestamp DATETIME2(6),
+
+    order_date DATE,
+
+    order_value_inr DECIMAL(10,2),
+
+    delivery_time_minutes INT,
+
+    product_category VARCHAR(100),
+
+    service_rating INT,
+
+    customer_sentiment VARCHAR(50),
+
+    refund_requested VARCHAR(10),
+
+    sla_breach_flag VARCHAR(10),
+
+    high_value_order_flag VARCHAR(20)
+);
+GO
+
+INSERT INTO dbo.fact_orders
+
 SELECT
-    order_id,
-    customer_id,
-    order_date,
-    order_timestamp,
-    platform,
-    product_category,
-    order_value_inr,
-    delivery_time_minutes,
-    service_rating,
-    delivery_delay,
-    refund_requested,
-    sla_breach_flag,
-    customer_sentiment
-FROM silver_orders;
+
+    so.order_id,
+
+    so.customer_id,
+
+    CAST(
+        ABS(CAST(CHECKSUM(so.order_id) AS BIGINT))
+        % 8680
+    AS INT) + 1 AS restaurant_key,
+
+    so.platform,
+
+    CAST(so.order_timestamp AS DATETIME2(6)),
+
+    so.order_date,
+
+    so.order_value_inr,
+
+    so.delivery_time_minutes,
+
+    so.product_category,
+
+    so.service_rating,
+
+    so.customer_sentiment,
+
+    CASE
+        WHEN ABS(CAST(CHECKSUM(so.order_id) AS BIGINT)) % 20 = 0
+        THEN 'Yes'
+        ELSE 'No'
+    END AS refund_requested,
+
+    CASE
+        WHEN so.delivery_time_minutes > 35
+        THEN 'Yes'
+        ELSE 'No'
+    END AS sla_breach_flag,
+
+    CASE
+        WHEN so.order_value_inr >= 1500
+        THEN 'High'
+        ELSE 'Standard'
+    END AS high_value_order_flag
+
+FROM dbo.silver_orders so;
+GO
